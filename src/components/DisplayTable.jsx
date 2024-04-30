e86e91e08503a9241c714850b83beefd4478dcdf
Brandon Cermak <cermakprofessional@gmail.com>
2024-04-29 19:25:50 -0500

    initial table done

--- src/components/DisplayTable.jsx
+++ src/components/DisplayTable.jsx
@@ -1,7 +1,19 @@
 import { connect } from "mongoose";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const DisplayTable = () => {
+
+    const [siteName, setSiteName] = useState("");
+    const [lcFlowRate, setLcFlowRate] = useState(0);
+    const [lcWaterDepth, setLcWaterDepth] = useState(0);
+    const [threeSixtyFlowRate, setThreeSixtyFlowRate] = useState(0);
+    const [threeSixtyWaterDepth, setThreeSixtyWaterDepth] = useState(0);
+    const [aboveBartonFlowRate, setAboveBartonFlowRate] = useState(0);
+    const [aboveWaterDepth, setAboveBartonWaterDepth] = useState(0);
+    const [bartonSpringsFlowRate, setBartonSpringsFlowRate] = useState(0);
+    const [bartonSpringsWaterDepth, setBartonSpringsWaterDepth] = useState(0);
+
+
 
     useEffect(() => {
         fetch('https://waterservices.usgs.gov/nwis/iv/?format=json&sites=08155240&siteStatus=all')
@@ -9,15 +21,100 @@ const DisplayTable = () => {
         .then((json) => {
             const data = json;
             const flowData = data.value.timeSeries[0];
-            console.log(flowData)
+            // console.log(flowData)
             const heightData = data.value.timeSeries[1];
-            console.log(heightData);
+            // console.log(heightData);
+            const siteName = heightData.sourceInfo.siteName;
+            setSiteName(siteName);
+            const flowRate = flowData.values[0].value[0].value;
+            setLcFlowRate(flowRate);
+            const waterDepth = heightData.values[0].value[0].value;
+            setLcWaterDepth(waterDepth)
+        })
+        )
+    }, [])
+
+    useEffect(() => {
+        fetch('https://waterservices.usgs.gov/nwis/iv/?format=json&sites=08155300&siteStatus=all')
+        .then((res) => res.json()
+        .then((json) => {
+            const data = json;
+            const flowData = data.value.timeSeries[0];
+            // console.log(flowData)
+            const heightData = data.value.timeSeries[1];
+            // console.log(heightData);
+            const flowRate = flowData.values[0].value[0].value;
+            setThreeSixtyFlowRate(flowRate);
+            const waterDepth = heightData.values[0].value[0].value;
+            setThreeSixtyWaterDepth(waterDepth)
+        })
+        )
+    }, [])
+
+    useEffect(() => {
+        fetch('https://waterservices.usgs.gov/nwis/iv/?format=json&sites=08155400&siteStatus=all')
+        .then((res) => res.json()
+        .then((json) => {
+            const data = json;
+            const flowData = data.value.timeSeries[0];
+            // console.log(flowData)
+            const heightData = data.value.timeSeries[1];
+            // console.log(heightData);
+            const flowRate = flowData.values[0].value[0].value;
+            setAboveBartonFlowRate(flowRate);
+            const waterDepth = heightData.values[0].value[0].value;
+            setAboveBartonWaterDepth(waterDepth)
+        })
+        )
+    }, [])
+
+    useEffect(() => {
+        fetch('https://waterservices.usgs.gov/nwis/iv/?format=json&sites=08155500&siteStatus=all')
+        .then((res) => res.json()
+        .then((json) => {
+            const data = json;
+            const flowData = data.value.timeSeries[0];
+            // console.log(flowData)
+            const heightData = data.value.timeSeries[1];
+            // console.log(heightData);
+            const flowRate = flowData.values[0].value[0].value;
+            setBartonSpringsFlowRate(flowRate);
+            const waterDepth = heightData.values[0].value[0].value;
+            setBartonSpringsWaterDepth(waterDepth)
         })
         )
     }, [])
 
   return (
-    <div>DisplayTable</div>
+    <>
+        <table>
+            <tr>
+                <th> Location </th>
+                <th> Flow Rate </th>
+                <th> Water Depth </th>
+            </tr>
+            <tr>
+                <td> Lost Creek </td>
+                <td> {lcFlowRate} cfps </td>
+                <td> {lcWaterDepth} feet </td>
+            </tr>
+            <tr>
+                <td> 360 </td>
+                <td> {threeSixtyFlowRate} cfps </td>
+                <td> {threeSixtyWaterDepth} feet </td>
+            </tr>
+            <tr>
+                <td> Above Barton Creek </td>
+                <td> {aboveBartonFlowRate} cfps </td>
+                <td> {aboveWaterDepth} feet </td>
+            </tr>
+            <tr>
+                <td> Barton Springs </td>
+                <td> {bartonSpringsFlowRate} cfps </td>
+                <td> {bartonSpringsWaterDepth} feet </td>
+            </tr>
+        </table>
+    </>
   )
 }
 
