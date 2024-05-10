2f38400bdc4a59f76d962e717b6ce1cb38b77d27
Brandon Cermak <cermakprofessional@gmail.com>
2024-05-09 20:31:26 -0500

    a

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ require('dotenv').config();
 import express from 'express';
 const app = express();
  
-// Serving static files (HTML, CSS, JS)
-app.use(express.static('public'));
-
 // Starting the server
 const port = process.env.PORT || 8080;
 app.listen(port, ()=> console.log(`listening on port ${port}...`))
