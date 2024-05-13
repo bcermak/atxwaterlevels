806650e3acc8b01f081310a4bd1ddb601b6760b7
Brandon Cermak <cermakprofessional@gmail.com>
2024-05-13 18:48:58 -0500

    footer/css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id='footer'>
+        <footer>
+            ATX Water Levels
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
